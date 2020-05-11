8d1646be8a7b8ba257335f32647478b666296149
Nick Machnik <nick.machnik@gmail.com>
2020-05-11 13:48:46 +0200

    Rename sampling to sample

--- expected_levenshtein/tests/test_sampling.py
+++ expected_levenshtein/tests/test_sampling.py
@@ -2,20 +2,20 @@
 
 import unittest
 import numpy as np
-import sampling
+import sample
 
 
 class TestLevenshteinSamplingFunctions(unittest.TestCase):
     def test_lev(self):
         self.assertEqual(
-            sampling._lev_jit(
+            sample._lev_jit(
                 np.array(['a', 'b', 'c']),
                 np.array(['a', 'a', 'a']))[-1, -1],
             2)
 
     def test_sample(self):
         av_l = round(
-            sampling.random_average_levenshtein(2, 1000, np.arange(2))[-1, -1])
+            sample.random_average_levenshtein(2, 1000, np.arange(2))[-1, -1])
         self.assertEqual(av_l, 1)
 
 
