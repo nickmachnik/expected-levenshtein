8e795dc6aa1185701f4dba046b750d50d8d66271
Nick Machnik <nick.machnik@gmail.com>
2020-05-11 15:40:12 +0200

    Test fitting

---
+++ expected_levenshtein/tests/test_fitting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import unittest
+import numpy as np
+import sample
+import fit
+
+
+class TestFitting(unittest.TestCase):
+    def test_sample(self):
+        av_l = sample.random_average_levenshtein(25, 1000, np.arange(2))
+        coeffs, mses = fit.model_average_levenshtein(av_l)
+        print(coeffs, mses)
+        self.assertEqual(len(coeffs[0]), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
